1b5d9e5fc2928bbfbd8fd8de9b12222c7ac1c904
MaratMikushkin <marat.miki@gmail.com>
2013-08-08 07:55:54 +0000

    add comment deleting feauture

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,17 @@
 class CommentsController < ApplicationController
+ 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+    #@comment = @post.comments.create(params[:comment])
+    @comment = @post.comments.create(params[:comment].permit(:title, :text))#?realy permit :title and :text
     redirect_to post_path(@post)
   end
+ 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+ 
 end
