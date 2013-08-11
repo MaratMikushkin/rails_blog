a44d5ae5bd37b007e0832dc1b12ee60f79b2a43f
vagrant <vagrant@precise64.(none)>
2013-08-11 20:45:57 +0000

    added tests for post_controller.rb

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,59 @@
-require 'test_helper'
-
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # called before every single test
+  setup :initialize_post
+ 
+  # called after every single test
+  def teardown
+    @post = nil
+  end
+ 
+  test "should init post" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create post" do
+    assert_difference('Post.count') do
+      post :create, post: {title: 'Some title'}
+    end
+   
+    assert_redirected_to post_path(assigns(:post))
+  end
+
+  test "should show post" do
+    get :show, id: @post.id
+    assert_response :success
+  end
+  
+  test "should show index" do
+    get :index
+    assert_response :success
+  end
+  
+  test "should edit post" do
+    get :edit, id: @post.id
+    assert_response :success
+  end
+ 
+  test "should update post" do
+    patch :update, id: @post.id, post: {}
+    assert_redirected_to post_path(assigns(:post))
+  end
+ 
+  test "should destroy post" do
+    assert_difference('Post.count', -1) do
+      delete :destroy, id: @post.id
+    end
+ 
+    assert_redirected_to posts_path
+  end
+ 
+  private
+ 
+  def initialize_post
+    @post = posts(:one)
+    @request.env["HTTP_AUTHORIZATION"] = "Basic " + \
+        Base64::encode64("root:123")
+  end
+ 
 end
